78c13815293830ad6f6facd2aeba983bdb27b3ae
DiegoLins10 <diegofernandeslins@gmail.com>
2021-11-20 16:09:15 -0300

    metodos para mudança de idioma

--- assets/js/main.js
+++ assets/js/main.js
@@ -206,3 +206,52 @@ Email.send({
 }).then(
   message => alert(message)
 );
+
+/*==================== TRANSLATE ====================*/ 
+var comboGoogleTradutor = null; //Varialvel global
+    
+        function googleTranslateElementInit() {
+            new google.translate.TranslateElement({
+                //pageLanguage: 'en',
+                includedLanguages: 'pt,en',
+                layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL
+            }, 'google_translate_element');
+    
+            comboGoogleTradutor = document.getElementById("google_translate_element").querySelector(".goog-te-combo");
+        }
+    
+        function changeEvent(el) {
+            if (el.fireEvent) {
+                el.fireEvent('onchange');
+            } else {
+                var evObj = document.createEvent("HTMLEvents");
+    
+                evObj.initEvent("change", false, true);
+                el.dispatchEvent(evObj);
+            }
+        }
+    
+        function trocarIdioma(sigla) {
+            if (comboGoogleTradutor) {
+                comboGoogleTradutor.value = sigla;
+                changeEvent(comboGoogleTradutor);//Dispara a troca
+            }
+        }
+            
+
+        function Botao1(botao) {
+            //alert("função pt");
+            trocarIdioma('pt');
+            botao.setAttribute("onclick","Botao2(this);");
+            let img = document.querySelector('#theme-button2');
+            img.setAttribute("src","assets/img/br.png");
+          }
+          
+        function Botao2(botao) {
+            //alert("função en");
+            trocarIdioma('en');           
+            botao.setAttribute("onclick","Botao1(this);");
+            let img2 = document.querySelector('#theme-button2');
+            img2.setAttribute("src","assets/img/en.png");               
+          }
+  
